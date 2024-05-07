5be82d8b55796d7dbc8417bc488f9de813b3633f
Eomjangheon <mpmpm0610@gmail.com>
2024-05-08 00:52:15 +0900

    Feat: 폭발의범위, 지속시간, 위치, 세기 조절기능 추가

--- Engine/Bomb.cpp
+++ Engine/Bomb.cpp
@@ -20,33 +20,33 @@ void Bomb::Update()
 
 	if (INPUT->GetButtonDown(KEY_TYPE::RBUTTON))
 	{
-		isBombActivate = true;
+		m_isBombActivate = true;
 	}
 	explosion();
 
 
 	for (auto col : *(GetRigidBody()->GetCollideEvent())) {
 		col->m_rb2->SetfrictionCoef(0.f);
-		col->m_rb2->SetLinearVelocity(*col->m_normal * 10000.f);
-		col->m_rb2->SetMaxSpeed(3000.f);
+		//col->m_rb2->SetLinearVelocity(*col->m_normal * m_bombPower);
+		col->m_rb2->SetMaxSpeed(m_bombPower);
 	}
 
 }
 
 void Bomb::explosion()
 {
-	if (!isBombActivate)
+	if (!m_isBombActivate)
 		return;
 
 	shared_ptr<BaseCollider> bs = GetGameObject()->GetCollider();
 	shared_ptr<RigidBody> rb = GetRigidBody();
 
-	rb->MoveTo(Vec3(3000.f, -100.f, 5000.f));
+	rb->MoveTo(m_bombPos);
 	rb->SetLinearVelocity(Vec3(0.f, 10000.f, 0.f));
 
 
-	bombTime +=  DELTA_TIME * 3.14;
-	float size =  sin(bombTime) * 2500;
+	m_bombTime += DELTA_TIME * 3.14 / m_bombMaxTime;
+	float size =  sin(m_bombTime) * m_bombSize;
 	
 
 
@@ -54,9 +54,9 @@ void Bomb::explosion()
 	bs->SetRadius(size/2);
 	//bombTime += DELTA_TIME;
 
-	if (bombTime > 3.14f) {
-		bombTime = 0.f;
-		isBombActivate = false;
+	if (m_bombTime > 3.14f) {
+		m_bombTime = 0.f;
+		m_isBombActivate = false;
 		//bs->SetRadius(0.f);
 		rb->MoveTo(Vec3(10000, 300, 5000));
 	}
