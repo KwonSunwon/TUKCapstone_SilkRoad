8ac270edd1558bd6dfb96fc1b351fca2e20e5723
Junb0 <junb0sday@gmail.com>
2024-05-07 21:17:17 +0900

    Test: 플레이어 두명 연결 및 동기화 성공

--- Engine/EnginePch.h
+++ Engine/EnginePch.h
@@ -214,7 +214,7 @@ public:								\
 
 #define CONST_BUFFER(type)	GEngine->GetConstantBuffer(type)
 
-#define SEND(T) GET_SINGLE(NetworkManager)->Send(reinterpret_pointer_cast<char[]>(T), sizeof(T));
+#define SEND(T) GET_SINGLE(NetworkManager)->Send(reinterpret_pointer_cast<char[]>(T), T->m_size);
 
 struct TransformParams
 {
--- Engine/NetworkPlayer.cpp
+++ Engine/NetworkPlayer.cpp
@@ -22,9 +22,11 @@ void NetworkPlayer::LateUpdate()
 void NetworkPlayer::ProcessPacket(shared_ptr<PlayerPacket> packet)
 {
 	shared_ptr<RigidBody> rb = GetRigidBody();
-	//rb->MoveTo(packet->m_position);
+	shared_ptr<Transform> transform = GetTransform();
+	rb->MoveTo(packet->m_position);
 	rb->SetLinearVelocity(packet->m_velocity);
-	rb->SetRotation(packet->m_rotation);
+	transform->SetLocalRotation(packet->m_rotation);
+	//rb->SetRotation(packet->m_rotation);
 	if (GetAnimator()->GetCurrentClipIndex() != packet->m_animationIndex)
 		GetAnimator()->Play(packet->m_animationIndex);
 }
